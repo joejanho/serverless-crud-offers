9fead4eecdd9921571fad97bcac1d7c6ef1f6212
joe janho <joejanho@gmail.com>
2021-05-16 14:27:48 +0200

    update: updating serverless.ts to include dynamo and env var

--- serverless.ts
+++ serverless.ts
@@ -1,31 +1,74 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
   service: 'serverlerss-offers',
   frameworkVersion: '2',
   custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true,
+    region: '${opt:region, self:provider.region}',
+    stage: '${opt:stage, self:provider.stage}',
+    list_table: '${self:service}-list-table-${opt:stage, self:provider.stage}',
+    tasks_table: '${self:service}-tasks-table-${opt:stage, self:provider.stage}',
+    table_throughputs: {
+      prod: 5,
+      default: 1,
     },
+    table_throughput: '${self:custom.TABLE_THROUGHPUTS.${self:custom.stage}, self:custom.table_throughputs.default}',
+    dynamodb: {
+      stages: ['dev'],
+      start: {
+        port: 8008,
+        inMemory: true,
+        heapInitial: '200m',
+        heapMax: '1g',
+        migrate: true,
+        seed: true,
+        convertEmptyValues: true,
+        // Uncomment only if you already have a DynamoDB running locally
+        // noStart: true
+      }
+    },
+    ['serverless-offline']: {
+      httpPort: 3000,
+      babelOptions: {
+        presets: ["env"]
+      }
+    }
   },
-  plugins: ['serverless-webpack'],
+  plugins: [
+      'serverless-bundle',
+      'serverless-offline',
+      'serverless-dotenv-plugin',
+  ],
+  package: {
+    individually: true,
+  },
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs12.x',
+    stage: 'dev',
+    region: 'eu-west-1',
     apiGateway: {
-      minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
+      minimumCompressionSize: 1024,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
-    lambdaHashingVersion: '20201221',
   },
-  // import the function via paths
-  functions: { hello },
-};
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          }
+        }
+      ]
+    }
+  },
+  
+}
 
 module.exports = serverlessConfiguration;
